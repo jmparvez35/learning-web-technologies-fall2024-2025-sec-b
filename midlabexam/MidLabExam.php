f5ae78bac229970b161a6d04955126e7d968bc18
jmparvez35 <jmparvez35@gmail.com>
2024-11-24 12:19:37 +0600

    Add files via upload

--- midlabexam/MidLabExam.php
+++ midlabexam/MidLabExam.php
@@ -1,1 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width-device-width" initial-scale="1.0">
+    <title>Document</title>
+</head>
 
+<body>
+    <form action="" method="POST";>
+        <label for="name" name="name">NAME</label><br>
+        <input type="text" name="name"><br>
+        <label for="email" name="email">EMAIL</label><br>
+        <input type="text" name="email"><br>
+        <label for="phone" name="phone">PHONE</label><br>
+        <input type="text" name="phone"><br>
+        <label for="paswword" name="password">PASSWORD</label><br>
+        <input type="text" name="password">
+</body>
+
+<table>
+    <th>GENDER</th>
+    <tr>
+        <td input type="radio" name="gender" value="male">MALE</td>
+        <td input type="radio" name="gender" value="female">FEMALE</td>
+        <td input type="radia" name>
+    </tr>
+</table>
